a19cc88a8cec58d4f608c404eb3d49ca2ed29bc2
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2022-01-25 09:54:19 +0900

    code update:file drag and drop

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,69 @@
 <script lang="ts">
-  export let name: string;
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+  import { escape } from "svelte/internal";
+
+  let _div: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+  let div = _div as unknown | SVGElement;
+  let ul: d3.Selection<HTMLUListElement, unknown, HTMLElement, any>;
+
+  onMount(() => {
+    _div = d3.select("div");
+
+    let div0 = _div.append("div");
+    div0
+      .attr("id", "drop_zone")
+      .text("ここにファイルをドロップ")
+      .on("dragover", function (event) {
+        handleDragOver(event);
+      })
+      .on("drop", function (event) {
+        handleDragFileDrop(event);
+      });
+
+    let output = _div.append("output");
+    output.attr("id", "file_list2");
+    ul = _div.append("ul");
+  });
+
+  function handleDragOver(event: DragEvent) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+  }
+
+  function handleDragFileDrop(event: DragEvent) {
+    console.log(event);
+    event.stopPropagation();
+    event.preventDefault();
+
+    Array.from(event.dataTransfer.files).forEach((f) => {
+      let li = ul.append("li");
+      li.append("strong").text(escape(f.name));
+      let span = li.append("span");
+      span.text(
+        " (" +
+          (f.type ? f.type : "n/a") +
+          ") - " +
+          f.size +
+          " bytes, last modified: " +
+          (f as any).lastModifiedDate.toLocaleDateString()
+      );
+    });
+  }
 </script>
 
-<template lang="pug">
- main 
-   h1 Hello {name}!
-   p Visit the 
-   |
-   |
-   a(href="https://svelte.dev/tutorial") Svelte tutorial
-   |
-   | to learn how to build Svelte apps.
-</template>
+<div bind:this={div} class="example" />
 
-<style src="./App.scss">
+<style lang="scss">
+  .example {
+    :global(#drop_zone) {
+      border: 2px dashed #bbb;
+      border-radius: 5px;
+      padding: 25px;
+      text-align: center;
+      font: 20pt bold "Vollkorn";
+      color: #bbb;
+    }
+  }
 </style>
